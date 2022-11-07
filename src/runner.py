a02b4875226f9213349263b46b74b43014426e0e
Kyle Nero <kylenero99@gmail.com>
2022-11-07 09:46:46 -0800

    2022 is now functioning

---
+++ src/runner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+import json
+
+from etl import get_data # todo
+
+def main():
+    print("todo")
+
+if __name__ == "__main__":
+    main()
+
